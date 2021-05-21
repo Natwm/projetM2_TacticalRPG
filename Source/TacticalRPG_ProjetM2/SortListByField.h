36f17346b68643fc0c2a82dc581c67237d31935f
natwPine <natw.mezza@hotmail.fr>
2021-05-21 18:52:34 +0200

    Test attaque feature

---
+++ Source/TacticalRPG_ProjetM2/SortListByField.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SortListByField.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TACTICALRPG_PROJETM2_API USortListByField : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
